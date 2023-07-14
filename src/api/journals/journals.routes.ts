18c4547463d3ceeb81c1d8cca7537be0d8ef2686
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-07-14 16:01:05 +0200

    Created journals routes and services.

---
+++ src/api/journals/journals.routes.ts
@@ -0,0 +1,113 @@
+import express, { NextFunction, Request, Response } from "express";
+
+import {
+  findAllJournals,
+  createJournal,
+  deleteJournal,
+  toggleFavoriteJournal,
+  updateJournal,
+} from "./journals.services";
+
+const router = express.Router();
+
+router.get("/all", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.payload.userId;
+
+    const journals = await findAllJournals(userId);
+
+    if (journals) {
+      res.json(journals);
+    } else {
+      res.json({
+        message: "No journals found",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post(
+  "/create",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.payload.userId;
+      const { title, text, date, favorite } = req.body;
+
+      await createJournal({
+        title,
+        text,
+        date,
+        favorite,
+        userId,
+      });
+
+      res.json({
+        status: 200,
+        message: "Journal created",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch(
+  "/update/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const { title, text } = req.body;
+
+      await updateJournal({
+        id,
+        title,
+        text,
+      });
+
+      res.json({
+        status: 200,
+        message: "Journal updated",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch(
+  "/favorite/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const journalId = req.params.id;
+
+      await toggleFavoriteJournal(journalId);
+
+      res.json({
+        status: 200,
+        message: "Journal updated",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  "/delete/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const journalId = req.params.id;
+
+      await deleteJournal(journalId);
+
+      res.json({
+        status: 200,
+        message: "Journal deleted",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
---
+++ src/api/journals/journals.services.ts
@@ -0,0 +1,67 @@
+import db from "../../utils/db";
+
+const findAllJournals = (userId: string) => {
+  return db.journal.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+};
+
+const createJournal = (journal: {
+  title: string;
+  text: string;
+  date: Date;
+  favorite: boolean;
+  userId: string;
+}) => {
+  return db.journal.create({
+    data: journal,
+  });
+};
+
+const deleteJournal = (id: string) => {
+  return db.journal.delete({
+    where: {
+      id: id,
+    },
+  });
+};
+
+const toggleFavoriteJournal = async (id: string) => {
+  const journal = await db.journal.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  return await db.journal.update({
+    where: {
+      id: id,
+    },
+    data: {
+      favorite: !journal?.favorite,
+    },
+  });
+};
+
+const updateJournal = (journal: {
+  id: string;
+  title: string;
+  text: string;
+}) => {
+  return db.journal.update({
+    where: {
+      id: journal.id,
+    },
+    data: journal,
+  });
+};
+
+export {
+  findAllJournals,
+  createJournal,
+  deleteJournal,
+  toggleFavoriteJournal,
+  updateJournal,
+};
